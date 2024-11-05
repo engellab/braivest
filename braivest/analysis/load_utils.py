2907df70b2a32f367cfe1909133c0723fd86a916
Julia <julwang@cshl.edu>
2024-11-05 15:26:25 -0500

    load model from file not from wandb

--- braivest/analysis/load_utils.py
+++ braivest/analysis/load_utils.py
@@ -1,5 +1,6 @@
 import wandb
 from braivest.model.emgVAE import emgVAE
+import yaml
 
 def load_wandb_model(run, input_dim, epoch=None):
     """
@@ -37,3 +38,18 @@ def load_wandb_model(run, input_dim, epoch=None):
                 raise       
     return model
 
+def load_model(model_path, config_path, input_dim):
+    """
+    Load a model from a path
+    Inputs:
+    -model_path (dtype:str): The path to the model
+    -config_path (dtype:str): The path to the config file
+    -input_dim (dtype:int): The input dimension of the model
+    """
+    with open(config_path, 'r') as f:
+        config = yaml.safe_load(f)
+    layers = [config['layer_dims'] for layer in range(config['num_layers'])]
+    model = emgVAE(input_dim, config['latent'], layers, config['kl'], emg = config['emg'])
+    model.build((None, input_dim))
+    model.load_weights(model_path)
+    return model
